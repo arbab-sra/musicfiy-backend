e78d5871123dde251530726eebadbe6662be787c
arbab-killer <mr.crazymemes76@gmail.com>
2024-06-10 11:20:15 +0530

    incress jwt token life

--- utils/jwtsigntoken.js
+++ utils/jwtsigntoken.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken"
 export const genratToken =(id)=>{
-const token =  jwt.sign({id :id},process.env.TWT_TOKEN_KEY ,{ expiresIn: '2d' }) //  2 days
+const token =  jwt.sign({id },process.env.TWT_TOKEN_KEY ,{ expiresIn: '15d' }) //  2 days
 return token
 }

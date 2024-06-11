0175e9280ade0d0d1728c3fe74bbc2fc43650c66
arbab-killer <mr.crazymemes76@gmail.com>
2024-06-11 23:34:57 +0530

    update

--- controlers/song/weekly_top_song.js
+++ controlers/song/weekly_top_song.js
@@ -1,15 +1,18 @@
 import { Song } from "../../models/song/songmodle.js";
 
-const weekly_top_songs = async (req,res)=>{
+const weekly_top_songs = async (req, res) => {
   try {
-        // Query the Song model and sort by views in descending order
-        const songs = await Song.find().sort({ views: -1 });
     
-        // Send the sorted songs as the response
-        res.status(200).json(songs);
-      } catch (error) {
-        // Handle any errors
-        res.status(500).json({ message: "Server Error", error });
-      }
-}
-export default weekly_top_songs
+    const severnday = new Date();
+    severnday.setDate(severnday.getDate() - 8);
+    // Query the Song model and sort by views in descending order
+    const songs = await  Song.find({ releaseDate: { $gte: severnday } }).sort({ views: -1 });
+
+    // Send the sorted songs as the response
+    res.status(200).json(songs);
+  } catch (error) {
+    // Handle any errors
+    res.status(500).json({ message: "Server Error", error });
+  }
+};
+export default weekly_top_songs;
--- routes/song/moodPlaylistRoute.js
+++ routes/song/moodPlaylistRoute.js
@@ -1,6 +1,10 @@
-import express from 'express';
-import { verifyToken } from '../../middleware/userverfy.js';
-import {mood_playlistControlar} from '../../controlers/song/index.js';
+import express from "express";
+import { verifyToken } from "../../middleware/userverfy.js";
+import { mood_playlistControlar } from "../../controlers/song/index.js";
 const router = express.Router();
-const mood_playlistRoute = router.get("/songs/moodplaylist" , verifyToken , mood_playlistControlar)
-export default mood_playlistRoute
+const mood_playlistRoute = router.get(
+  "/songs/moodplaylist",
+  verifyToken,
+  mood_playlistControlar
+);
+export default mood_playlistRoute;

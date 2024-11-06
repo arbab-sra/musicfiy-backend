affab42aff9e870fdbf8fff6cadf89086ab84175
arbab-killer <mr.crazymemes76@gmail.com>
2024-11-06 22:26:14 +0530

    modefy search quary controler

--- controlers/song/searchsong.js
+++ controlers/song/searchsong.js
@@ -1,4 +1,5 @@
 import { Song } from "../../models/song/songmodle.js";
+import Videosong from "../../models/videosong/videosong.js";
 const songBySearchingControllar = async (req, res) => {
   try {
     // Extract the search query from request parameters
@@ -11,9 +12,10 @@ const songBySearchingControllar = async (req, res) => {
     }
 
     // Use Mongoose to find songs where title or description matches the query
+
     const searchResults = await Song.find({
       $or: [
-        { title: { $regex: query, $options: "i" } },//
+        { title: { $regex: query, $options: "i" } }, //
         { description: { $regex: query, $options: "i" } },
         { mood: { $regex: query, $options: "i" } },
         {
@@ -25,8 +27,38 @@ const songBySearchingControllar = async (req, res) => {
       ],
     }).sort({ title: 1, description: 1, artist: 1, mood: 1, songCategory: 1 });
 
-    // Send the search results as the response
-    res.status(200).json(searchResults);
+    
+    if (searchResults.length === 0) {
+      const searchResultsVideo = await Videosong.find({
+        $or: [
+          { title: { $regex: query, $options: "i" } }, //
+          { description: { $regex: query, $options: "i" } },
+          { mood: { $regex: query, $options: "i" } },
+          {
+            songCategory: { $regex: query, $options: "i" },
+          },
+          {
+            artist: { $regex: query, $options: "i" },
+          },
+        ],
+      }).sort({
+        title: 1,
+        description: 1,
+        artist: 1,
+        mood: 1,
+        songCategory: 1,
+      });
+
+      if (searchResultsVideo.length === 0) {
+        return res.status(404).json({
+          message: "No results found",
+        });
+      }else{
+        return res.status(200).json(searchResultsVideo);
+      }
+    } else {
+      return res.status(200).json(searchResults);
+    }
   } catch (error) {
     // Handle any errors
     res.status(500).json({ message: "Server Error", error: error.message });
